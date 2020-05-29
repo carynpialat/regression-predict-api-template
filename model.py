73681badaca38dd0bdd156256d3fc29ea530a145
Caryn <caryn.oates@gmail.com>
2020-05-29 10:17:22 +0200

    debugging

--- model.py
+++ model.py
@@ -245,9 +245,10 @@ def _preprocess_data(data):
 
     df = df.set_index('Order No')
 
-    #Format dataset to have the correct columns
-    df = df.drop(['Placement - Time', 'Confirmation - Time', 'Arrival at Pickup - Time', 'Pickup - Time','User Id', 'Pickup Lat', 'Pickup Long', 'Destination Lat', 'Destination Long', 'Rider Id', 'weighted_rating','No_of_Ratings'], axis=1)
     print(df.columns)
+    #Format dataset to have the correct columns
+    df = df.drop(['Placement - Time', 'Confirmation - Time', 'Arrival at Pickup - Time', 'Pickup - Time','User Id', 'Pickup Lat', 'Pickup Long', 'Destination Lat', 'Destination Long', 'Rider Id', 'weighted_rating'], axis=1)
+    
     #Create the matrix of features.
     X_test = df.values
     # ------------------------------------------------------------------------

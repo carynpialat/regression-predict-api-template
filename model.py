6b57146c029512286fbab87b5c5fa82fe70cd34e
carynpialat <53856072+carynpialat@users.noreply.github.com>
2020-06-02 11:40:51 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -178,32 +178,14 @@ def _preprocess_data(data):
     df['time_AP-C'] = (df['Arrival at Pickup - Time'] - df['Confirmation - Time']).astype('timedelta64[s]').astype(np.int64)
     df['time_P-AP'] = (df['Pickup - Time'] - df['Arrival at Pickup - Time']).astype('timedelta64[s]').astype(np.int64)
 
-    #Drop rows that has negative time intervals (eg. not possible for the confirmation to happen before the order is placed)
-    ls = [col for col in df if col.startswith('time')]
-    for i in range(len(ls)):
-        df = df.drop(df[df[ls[i]] <= 0].index)
-
     #Calculate time in seconds from midnight
     df['pl'] = df['Placement - Time']. apply(lambda x: (x - pd.to_datetime('12:00:00 AM', format='%I:%M:%S %p')).total_seconds())
     df['con'] = df['Confirmation - Time']. apply(lambda x: (x - pd.to_datetime('12:00:00 AM', format='%I:%M:%S %p')).total_seconds())
     df['arr p'] = df['Arrival at Pickup - Time']. apply(lambda x: (x - pd.to_datetime('12:00:00 AM', format='%I:%M:%S %p')).total_seconds())
     df['p'] = df['Pickup - Time']. apply(lambda x: (x - pd.to_datetime('12:00:00 AM', format='%I:%M:%S %p')).total_seconds())
 
-    #sin/cos transformation of time values
-    ls = ['pl', 'con', 'arr p', 'p']
-    ls_sin = ['pl_sin', 'con_sin', 'arr p_sin', 'p_sin']
-    ls_cos = ['pl_cos', 'con_cos', 'arr p_cos', 'p_cos']
-    for i in range(len(ls)):
-        df[ls_sin[i]] = df[ls[i]].apply(lambda x: np.sin(x*(2.*np.pi/86400)))
-        df[ls_cos[i]] = df[ls[i]].apply(lambda x: np.cos(x*(2.*np.pi/86400)))
-
-    #sin/cos transform 'Weekday'
-    df['weekday_sin'] = df['Pickup - Weekday (Mo = 1)'].apply(lambda x: np.sin(x*(2.*np.pi/7)))
-    df['weekday_cos'] = df['Pickup - Weekday (Mo = 1)'].apply(lambda x: np.cos(x*(2.*np.pi/7)))
-
-    #sin/cos transform 'Day of Month'
-    df['day_month_sin'] = df['Pickup - Day of Month']. apply(lambda x: np.sin(x*(2.*np.pi/31)))
-    df['day_month_cos'] = df['Pickup - Day of Month']. apply(lambda x: np.cos(x*(2.*np.pi/31)))
+    #Extract hour that rider picked up the shipment
+    df['pickup_hour'] = df['Placement - Time'].apply(lambda x: x.hour)
 
     #Rank riders by weighted rating value and efficiency
     total = sum(riders['No_of_Ratings'])
@@ -211,7 +193,7 @@ def _preprocess_data(data):
     df['deliveries_per_day'] = df['No_Of_Orders'] / df['Age']
 
     model_features = ['User Id', 'dest_geohash', 'pickup_geohash', 'time_C-Pl', 'time_AP-C', 'time_P-AP', 'Distance (KM)', 'Pickup - Day of Month', 'Pickup - Weekday (Mo = 1)', 'pl', 'con', 'arr p', 'p',
-                        'weekday_sin', 'weekday_cos', 'day_month_sin', 'day_month_cos', 'ranking', 'deliveries_per_day', 'pl_sin', 'con_sin', 'arr p_sin', 'p_sin', 'pl_cos', 'con_cos', 'arr p_cos', 'p_cos']
+                        'ranking', 'deliveries_per_day', 'pickup_hour']
 
     df = df[model_features]
 
